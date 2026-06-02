434b2bfe2f4d7fd7ae09c0616c8e2ea4a68b041f
Nico Auerswald <auersw.nico@gmail.com>
2026-06-02 18:59:00 +0200

    move image definition in own file

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,7 @@
-// raylib-zig (c) Nikolas Wipper 2023
-
-const rl = @import("raylib");
-
-const RLImage = struct {
-    image: rl.Image,
-
-    pub fn init(width: i32, height: i32) RLImage {
-        return RLImage{
-            .image = rl.Image.genColor(width, height, rl.Color.black),
-        };
-    }
-
-    pub fn deinit(self: RLImage) void {
-        self.image.unload();
-    }
-
-    pub fn export_image(self: RLImage, filename: [:0]const u8) bool {
-        return rl.exportImage(self.image, filename);
-    }
-};
+const image = @import("image.zig");
 
 pub fn main() anyerror!void {
-    const img = RLImage.init(64, 64);
+    const img = image.RLImage.init(64, 64);
     defer img.deinit();
 
     _ = img.export_image("output.png");
