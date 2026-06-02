9f66005c3f17ec65dbb388d43ca38bee7dba9c38
Nico Auerswald <auersw.nico@gmail.com>
2026-06-02 19:14:50 +0200

    implement setting pixels

--- src/image.zig
+++ src/image.zig
@@ -9,8 +9,12 @@ pub const RLImage = struct {
         };
     }
 
-    pub fn deinit(self: RLImage) void {
+    pub fn deinit(self: *RLImage) void {
         self.image.unload();
+    }
+
+    pub fn set_pixel(self: *RLImage, x: i32, y: i32) void {
+        self.image.drawPixel(x, y, rl.Color.red);
     }
 
     pub fn export_image(self: RLImage, filename: [:0]const u8) bool {
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,13 @@
 const image = @import("image.zig");
 
 pub fn main() anyerror!void {
-    const img = image.RLImage.init(64, 64);
+    var img = image.RLImage.init(64, 64);
     defer img.deinit();
+
+    const ax = 7;
+    const ay = 3;
+
+    img.set_pixel(ax, ay);
 
     _ = img.export_image("output.png");
 }
